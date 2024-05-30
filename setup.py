fb374386c801a5eff8c1a592449e708b34c6ca9d
Martin Rodrigue Atangana Ongolo <71257342+Martingeek4life@users.noreply.github.com>
2024-05-30 18:19:41 +0200

    Update setup.py reinit

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@
 # This source code is licensed under the license found in the LICENSE file in
 # the root directory of this source tree. An additional grant of patent rights
 # can be found in the PATENTS file in the same directory.
+#
 
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_py import build_py
 import sys
-from torch.utils.cpp_extension import CppExtension, BuildExtension, CUDAExtension
+from torch.utils.ffi import create_extension
+
 
 if sys.version_info < (3,):
     sys.exit('Sorry, Python3 is required for fairseq.')
@@ -23,8 +25,8 @@ with open('LICENSE') as f:
 with open('requirements.txt') as f:
     reqs = f.read()
 
-bleu = CppExtension(
-    name='fairseq.libbleu',
+bleu = Extension(
+    'fairseq.libbleu',
     sources=[
         'fairseq/clib/libbleu/libbleu.cpp',
         'fairseq/clib/libbleu/module.cpp',
@@ -32,20 +34,23 @@ bleu = CppExtension(
     extra_compile_args=['-std=c++11'],
 )
 
-conv_tbc = CUDAExtension(
-    name='fairseq.temporal_convolution_tbc',
-    sources=[
-        'fairseq/clib/temporal_convolution_tbc/temporal_convolution_tbc.cpp',
-    ],
-    include_dirs=[],  # Add necessary include directories here
+conv_tbc = create_extension(
+    'fairseq.temporal_convolution_tbc',
+    relative_to='fairseq',
+    headers=['fairseq/clib/temporal_convolution_tbc/temporal_convolution_tbc.h'],
+    sources=['fairseq/clib/temporal_convolution_tbc/temporal_convolution_tbc.cpp'],
     define_macros=[('WITH_CUDA', None)],
+    with_cuda=True,
     extra_compile_args=['-std=c++11'],
+    source_extension='.cpp',
 )
+
 
 class build_py_hook(build_py):
     def run(self):
-        self.run_command("build_ext")
-        super().run()
+        conv_tbc.build()
+        build_py.run(self)
+
 
 setup(
     name='fairseq',
@@ -55,13 +60,14 @@ setup(
     license=license,
     install_requires=reqs.strip().split('\n'),
     packages=find_packages(),
-    ext_modules=[bleu, conv_tbc],
-    cmdclass={
-        'build_py': build_py_hook,
-        'build_ext': BuildExtension
-    },
+    ext_modules=[bleu],
+
+    # build and install PyTorch extensions
     package_data={
-        'fairseq': ['clib/temporal_convolution_tbc/*.so'],
+        'fairseq': ['temporal_convolution_tbc/*.so'],
     },
     include_package_data=True,
+    cmdclass={
+        'build_py': build_py_hook,
+    },
 )
